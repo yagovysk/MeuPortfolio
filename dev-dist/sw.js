15b221b42b5f646f21e0cc2a0c065bc974987730
yagovysk <yago09regis@gmail.com>
2026-02-23 15:42:08 -0300

    aumentando a foto de perfil

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-b004d73f'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.aaa973m7vn"
+    "revision": "0.3qf8a6l7ibg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
